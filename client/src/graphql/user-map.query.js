be1a5263dc1a5aa0b731370e790f2c2fa20bbc54
rostorres <roselle.dnlc@gmail.com>
2019-01-31 15:58:38 +0100

    fixing MapView

---
+++ client/src/graphql/user-map.query.js
@@ -0,0 +1,84 @@
+import gql from 'graphql-tag';
+// get the user and all user's groups
+import MESSAGE_FRAGMENT from './message.fragment';
+
+export const USER_MAP_QUERY = gql`
+  query user($id: Int) {
+    user(id: $id) {
+      id
+      email
+      username
+      age
+      gender
+      civilStatus
+      children
+      city
+      country
+      likes
+      street
+      streetNumber
+      zipcode
+      birthdate
+      height
+      weight
+      education
+      profession
+      religion
+      pets
+      smoker
+      description
+      photoprofile {
+        id
+        url
+      }
+      groups {
+        id
+        name
+        messages(messageConnection: { first: 1 }) {
+          # we don't need to use variables
+          edges {
+            cursor
+            node {
+              ...MessageFragment
+            }
+          }
+        }
+        photo
+        users {
+          id
+          photoprofile {
+            id
+            url
+          }
+        }
+      }
+      album {
+        id
+        url
+      }
+      friends {
+        id
+        username
+        photoprofile {
+          id
+          url
+        }
+      }
+      miscreated {
+        id
+        username
+      }
+      searches {
+        id
+        gender
+        civilStatus
+        children
+        userId {
+          id
+        }
+      }
+    }
+  }
+  ${MESSAGE_FRAGMENT}
+`;
+export default USER_MAP_QUERY;
--- client/src/screens/near.screen.js
+++ client/src/screens/near.screen.js
@@ -5,11 +5,11 @@ import {
 import { graphql, compose } from 'react-apollo';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import Geocoder from 'react-native-geocoder';
-import { USERS_QUERY } from '../graphql/users.query';
-import { USER_QUERY } from '../graphql/user.query';
+import { USERS_MAP_QUERY } from '../graphql/users-map.query';
+import { USER_MAP_QUERY } from '../graphql/user-map.query';
 import withLoading from '../components/withLoading';
 
-/*const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
@@ -26,7 +26,6 @@ import withLoading from '../components/withLoading';
 class SingleMarker extends Component {
     constructor(props) {
         super(props);
-        //console.log(item.username, ": ", item.city);
         this.state = {
             lng: null,
             lat: null,
@@ -37,24 +36,21 @@ class SingleMarker extends Component {
         const { item } = this.props;
         Geocoder.geocodeAddress(item.city, ", España")
             .then((res) => {
-                //console.log(item.username, ": ", item.city);
-                //console.log(user.city, ": ", res[0].position.lng);
                 if (res[0].position.lng) {
                     this.setState({ lng: res[0].position.lng });
-                    //console.log('aqui');
                 } else {
                     this.setState({ lng: null });
-                    //console.log('ali');
+                    console.log("error: ", item.username, ": ", item.city);
                 }
                 if (res[0].position.lat) {
                     this.setState({ lat: res[0].position.lat });
                 } else {
-                    this.setState({ lat: null });
+                    console.log("error: ", item.username, ": ", item.city);
                 }
-                //console.log(this.state.lng);
-                //console.log(this.state.lat);
             })
-            .catch(err => console.log(err));
+            .catch(err => {
+                console.log(err);
+            });
     }
 
     goToProfile = item => () => {
@@ -69,8 +65,7 @@ class SingleMarker extends Component {
     render() {
         const { item } = this.props;
         const { lng, lat } = this.state;
-        console.log(item.username, ": ", lng, lat);
-        //console.log(lng);
+        console.log(item.id, item.username, ": ", lng, lat);
         if (lng && lat) {
             return (
                 <Marker
@@ -97,10 +92,8 @@ class UserMarker extends Component {
     }
 
     render() {
-        const { user, lng, lat } = this.props;;
-        //console.log(lat);
-        //console.log(lng);
-        //console.log(user.username);
+        const { user, lng, lat } = this.props;
+        //console.log(user.id, user.username, ": ", lng, lat);
         if (lng && lat && user.username) {
             return (
                 <Marker
@@ -133,28 +126,25 @@ class Nearer extends Component {
             userLatitude: null,
             userLongitude: null,
         };
-        //console.log(user.city);
+        console.log(user.city);
         Geocoder.geocodeAddress(user.city, ", España")
             .then((res) => {
-                //console.log(user.city, ": ", res[0].position.lng);
                 if (res[0].position.lng) {
                     this.setState({ longitude: res[0].position.lng });
                     this.setState({ userLongitude: res[0].position.lng });
-                    //console.log('aqui');
+                } else {
+                    console.log("error: ", user.username, ": ", user.city);
                 }
                 if (res[0].position.lat) {
                     this.setState({ latitude: res[0].position.lat });
                     this.setState({ userLatitude: res[0].position.lat });
+                } else {
+                    console.log("error: ", user.username, ": ", user.city);
                 }
-                //console.log(user.city);
-                //console.log(this.state.longitude);
-                //console.log(this.state.latitude);
-                //console.log(this.state.longitudeDelta);
-                //console.log(this.state.latitudeDelta);
-                //console.log(this.state.userLongitude);
-                //console.log(this.state.userLatitude);
             })
-            .catch(err => console.log(err));
+            .catch(err => {
+                console.log(err);
+            });
     }
 
     /*getPosition = () => {
@@ -165,64 +155,86 @@ class Nearer extends Component {
             })
             .catch(err => console.log(err));
     };*/
-/*render() {
-    const { user, users } = this.props;
-    return (
-        <View style={styles.container}>
-            <MapView
-                provider={PROVIDER_GOOGLE}
-                style={styles.map}
-                initialRegion={{
-                    latitude: this.state.latitude,
-                    longitude: this.state.longitude,
-                    latitudeDelta: this.state.latitudeDelta,
-                    longitudeDelta: this.state.longitudeDelta,
-                }}
-                zoomEnabled
-            //onPress={this.getPosition}
-            >
-                <UserMarker user={user} lng={this.state.userLongitude} lat={this.state.userLatitude} />
-                {
-                    users.filter(item => item.id !== user.id).map((item, index) => (
-                        <SingleMarker key={index} item={item} properties={this.props} />
-                    ))
-                }
-            </MapView>
-        </View>
-    );
-}
+    userFilter = item => {
+        const { user } = this.props;
+        if (user) {
+            return item.id !== user.id;
+        } else {
+            1 === 1;
+        }
+    }
+
+    userKey = () => {
+        const { user } = this.props;
+        if (user) {
+            return user.id.toString();
+        } else {
+            return '1';
+        }
+    }
+
+    //<UserMarker key={this.userKey} user={user} lng={this.state.userLongitude} lat={this.state.userLatitude} />
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <View style={styles.container}>
+                <MapView
+                    provider={PROVIDER_GOOGLE}
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: this.state.latitude,
+                        longitude: this.state.longitude,
+                        latitudeDelta: this.state.latitudeDelta,
+                        longitudeDelta: this.state.longitudeDelta,
+                    }}
+                    zoomEnabled
+                //onPress={this.getPosition}
+                >
+                    {
+                        user ? <UserMarker key={this.userKey} user={user} lng={this.state.userLongitude} lat={this.state.userLatitude} /> : <View></View>
+                    }
+
+                    {
+                        users.filter(this.userFilter).map((item) => (
+                            <SingleMarker key={item.id} item={item} properties={this.props} />
+                        ))
+                    }
+                </MapView>
+            </View>
+        );
+    }
 }
 
-const userQuery = graphql(USER_QUERY, {
-options: () => ({
-    variables: {
-        id: 1,
-    },
-}),
-props: ({ data: { loading, user } }) => ({
-    loading,
-    user,
-}),
+const userQuery = graphql(USER_MAP_QUERY, {
+    options: () => ({
+        variables: {
+            id: 1,
+        },
+    }),
+    props: ({ data: { loading, user } }) => ({
+        loading,
+        user,
+    }),
 });
 
-const usersQuery = graphql(USERS_QUERY, {
-options: () => ({}), // fake the user for now
-props: ({ data: { users } }) => ({
-    users: users || [],
-}),
+const usersQuery = graphql(USERS_MAP_QUERY, {
+    options: () => ({}), // fake the user for now
+    props: ({ data: { users } }) => ({
+        users: users || [],
+    }),
 });
 
 export default compose(
-userQuery,
-usersQuery,
-withLoading,
+    userQuery,
+    usersQuery,
+    withLoading,
 )(Nearer);
-*/
 
-const Nearer = () => (
+/*const Nearer = () => (
     <View>
         <Text>Fixing</Text>
     </View>
 );
 
-export default Nearer;
+export default Nearer;*/
